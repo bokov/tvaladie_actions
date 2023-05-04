4999d3972577bab848d2a1c75710de83dde14dde
tvaladie <valadie@uthscsa.edu>
2023-05-04 11:49:47 -0500

    5/4 Folder cleanup, Gitignore, docker and deploy

--- BexarCovid/server.R
+++ BexarCovid/server.R
@@ -52,13 +52,4 @@ shinyServer(function(input, output) {
     #observeEvent(event_data('plotly_selected'),{browser()})
 })
 
-#renderUI for matching number of variables to colors#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 #renderUI for matching number of variables to colors
---
+++ GGHelper/GGhelper.R
@@ -0,0 +1,10 @@
+# GGhelper
+#
+# Using data(x) and filling in iris, cars, etc ensures that a particular dataset is available
+# How to get available datasets: data() - the human-readable version 
+# data()$results - the data.frame version so you can use code on it
+#
+# Start out with a working webapp even if it doesn't do anything and then try and add functionality to it
+# rather than trying to make functions work and then turn it into an app
+#
+# Get the functionality to work in the console and then when it works paste it in
